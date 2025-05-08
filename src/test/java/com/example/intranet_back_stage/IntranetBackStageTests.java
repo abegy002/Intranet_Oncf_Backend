601ecde762b564a0d382a2388eae9fc5001b6a19
AYOUB EL GHAYTHY <ayoub.elghaythy@gmail.com>
2025-05-08 10:40:38 +0100

    Getting Started

---
+++ src/test/java/com/example/intranet_back_stage/IntranetBackStageTests.java
@@ -0,0 +1,13 @@
+package com.example.intranet_back_stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetBackStageTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
