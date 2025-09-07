2032b197e49331dc008110fce14fdf5951f23fef
nasserghaythman63 <nasserghaythman63@gmail.com>
2025-09-07 17:53:33 +0100

    change password and user status added 2

--- src/main/java/com/example/intranet_back_stage/security/NotificationWebSocketConfig.java
+++ src/main/java/com/example/intranet_back_stage/security/NotificationWebSocketConfig.java
@@ -1,27 +1,27 @@
-package com.example.intranet_back_stage.security;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class NotificationWebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // SockJS fallback endpoint
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        // Clients subscribe to /user/queue/notifications (user-specific)
-        config.enableSimpleBroker("/topic", "/queue");
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user");
-    }
-}
+//package com.example.intranet_back_stage.security;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.*;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class NotificationWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // SockJS fallback endpoint
+//        registry.addEndpoint("/ws")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
+//    }
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        // Clients subscribe to /user/queue/notifications (user-specific)
+//        config.enableSimpleBroker("/topic", "/queue");
+//        config.setApplicationDestinationPrefixes("/app");
+//        config.setUserDestinationPrefix("/user");
+//    }
+//}
